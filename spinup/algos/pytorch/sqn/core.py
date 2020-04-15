c56b590e2d42edd3a193363f7e0c85948cfb6654
christopher-hsu <christopherhsu8@gmail.com>
2020-04-15 13:17:47 -0400

    clean up and move process_obs

--- spinup/algos/pytorch/sqn/core.py
+++ spinup/algos/pytorch/sqn/core.py
@@ -22,50 +22,74 @@ def mlp(sizes, activation, output_activation=nn.Identity):
 def count_vars(module):
     return sum([np.prod(p.shape) for p in module.parameters()])
 
+def process_obs(obs):
 
-LOG_STD_MAX = 2
-LOG_STD_MIN = -20
+    '''
+        Args: 
+            obs: observation that we get out of each step
+        Returns:
+            obs_array: 15 features extracted that include: ego_pose,opp_pose, 3 closest points using lidar. All of the
+            features are in global frame (numpy array shape (16,))
+    '''
 
-class SquashedGaussianMLPActor(nn.Module):
+    num_smallest = 3
 
-    def __init__(self, obs_dim, act_dim, hidden_sizes, activation):#, act_limit):
-        super().__init__()
-        self.net = mlp([obs_dim] + list(hidden_sizes), activation, activation)
-        self.mu_layer = nn.Linear(hidden_sizes[-1], act_dim)
-        self.log_std_layer = nn.Linear(hidden_sizes[-1], act_dim)
-        # self.act_limit = act_limit
+    obs_array = np.zeros((10 + (num_smallest*2),))
 
-    def forward(self, obs, deterministic=False, with_logprob=True):
-        net_out = self.net(obs)
-        mu = self.mu_layer(net_out)
-        log_std = self.log_std_layer(net_out)
-        log_std = torch.clamp(log_std, LOG_STD_MIN, LOG_STD_MAX)
-        std = torch.exp(log_std)
+    obs_array[0] = obs['poses_x'][0] # ego pos_x
+    obs_array[1] = obs['poses_y'][0] # ego pos_y
+    obs_array[2] = obs['poses_theta'][0] # ego theta
 
-        # Pre-squash distribution and sample
-        pi_distribution = Normal(mu, std)
-        if deterministic:
-            # Only used for evaluating policy at test time.
-            pi_action = mu
-            # pi_action = torch.argmax(mu,dim=1)
-        else:
-            pi_action = pi_distribution.rsample()
+    R_mat_ego = np.array([[np.cos(obs_array[2]),np.sin(obs_array[2])],
+                        [-np.sin(obs_array[2]),np.cos(obs_array[2])]])
 
-        if with_logprob:
-            # Compute logprob from Gaussian, and then apply correction for Tanh squashing.
-            # NOTE: The correction formula is a little bit magic. To get an understanding 
-            # of where it comes from, check out the original SAC paper (arXiv 1801.01290) 
-            # and look in appendix C. This is a more numerically-stable equivalent to Eq 21.
-            # Try deriving it yourself as a (very difficult) exercise. :)
-            logp_pi = pi_distribution.log_prob(pi_action).sum(axis=-1)
-            logp_pi -= (2*(np.log(2) - pi_action - F.softplus(-2*pi_action))).sum(axis=1)
-        else:
-            logp_pi = None
+    ego_vel_car_frame = np.zeros((1,2))
 
-        pi_action = torch.tanh(pi_action)
-        # pi_action = self.act_limit * pi_action
+    ego_vel_car_frame[0,0] = obs['linear_vels_x'][0]
+    ego_vel_car_frame[0,1] = 0
 
-        return pi_action, logp_pi
+    ego_vel_global = np.dot(ego_vel_car_frame,R_mat_ego)
+
+    obs_array[3:5] = ego_vel_global[0,:] # ego velocity x and y
+
+    obs_array[5] = obs['poses_x'][1] # opp pos_x
+    obs_array[6] = obs['poses_y'][1] # opp pos_y
+    obs_array[7] = obs['poses_theta'][1] # opp theta
+
+    R_mat_opp = np.array([[np.cos(obs_array[7]),np.sin(obs_array[7])],
+                        [-np.sin(obs_array[7]),np.cos(obs_array[7])]])
+
+    opp_vel_car_frame = np.zeros((1,2))
+
+    opp_vel_car_frame[0,0] = obs['linear_vels_x'][1]
+
+    opp_vel_car_frame[0,1] = 0
+
+    opp_vel_global = np.dot(ego_vel_car_frame,R_mat_opp)
+
+    obs_array[8:10] = opp_vel_global[0,:] # opp velocity x and y
+
+    ### Process the lidar data to get the postion of the closest 3 points:
+
+    ranges = np.array(list(obs['scans'][0]))
+    angles = np.linspace(-4.7/2., 4.7/2. num=ranges.shape[0])
+
+    ranges = ranges[np.isfinite(ranges)]
+    angles = angles[np.isfinite(ranges)]
+
+    k_smallest_idx = np.argpartition(ranges,num_smallest)
+    k_smallest_idx = k_smallest_idx[:num_smallest]
+
+    lidar_xy_car_frame = np.zeros((num_smallest,2))
+
+    lidar_xy_car_frame[:,0] = (ranges[k_smallest_idx] * np.cos(angles[k_smallest_idx]))
+    lidar_xy_car_frame[:,1] = (ranges[k_smallest_idx] * np.sin(angles[k_smallest_idx]))
+
+    lidar_xy_global = np.dot(lidar_xy_car_frame,R_mat_ego)
+
+    obs_array[10:] = lidar_xy_global.flatten()
+
+    return obs_array
 
 class MLPActionSelector(nn.Module):
 
@@ -77,7 +101,8 @@ class MLPActionSelector(nn.Module):
 
     def action(self, q, deterministic=False, with_logprob=True):
 
-        pi_log = self.log_softmax(q/self.alpha)
+        # pi_log = self.log_softmax(q/self.alpha)
+        soft_q = q/self.alpha
 
         if deterministic:
             mu = torch.argmax(pi_log)
--- spinup/algos/pytorch/sqn/sqn.py
+++ spinup/algos/pytorch/sqn/sqn.py
@@ -41,74 +41,6 @@ class ReplayBuffer:
         return {k: torch.as_tensor(v, dtype=torch.float32) for k,v in batch.items()}
 
 
-def process_obs(obs):
-
-    '''
-        Args: 
-            obs: observation that we get out of each step
-        Returns:
-            obs_array: 15 features extracted that include: ego_pose,opp_pose, 3 closest points using lidar. All of the
-            features are in global frame (numpy array shape (16,))
-    '''
-
-    num_smallest = 3
-
-    obs_array = np.zeros((10 + (num_smallest*2),))
-
-    obs_array[0] = obs['poses_x'][0] # ego pos_x
-    obs_array[1] = obs['poses_y'][0] # ego pos_y
-    obs_array[2] = obs['poses_theta'][0] # ego theta
-
-    R_mat_ego = np.array([[np.cos(obs_array[2]),np.sin(obs_array[2])],
-                        [-np.sin(obs_array[2]),np.cos(obs_array[2])]])
-
-    ego_vel_car_frame = np.zeros((1,2))
-
-    ego_vel_car_frame[0,0] = obs['linear_vels_x'][0]
-    ego_vel_car_frame[0,1] = 0
-
-    ego_vel_global = np.dot(ego_vel_car_frame,R_mat_ego)
-
-    obs_array[3:5] = ego_vel_global[0,:] # ego velocity x and y
-
-    obs_array[5] = obs['poses_x'][1] # opp pos_x
-    obs_array[6] = obs['poses_y'][1] # opp pos_y
-    obs_array[7] = obs['poses_theta'][1] # opp theta
-
-    R_mat_opp = np.array([[np.cos(obs_array[7]),np.sin(obs_array[7])],
-                        [-np.sin(obs_array[7]),np.cos(obs_array[7])]])
-
-    opp_vel_car_frame = np.zeros((1,2))
-
-    opp_vel_car_frame[0,0] = obs['linear_vels_x'][1]
-
-    opp_vel_car_frame[0,1] = 0
-
-    opp_vel_global = np.dot(ego_vel_car_frame,R_mat_opp)
-
-    obs_array[8:10] = opp_vel_global[0,:] # opp velocity x and y
-
-    ### Process the lidar data to get the postion of the closest 3 points:
-
-    ranges = np.array(list(obs['scans'][0]))
-    angles = np.linspace(-4.7/2., 4.7/2. num=ranges.shape[0])
-
-    ranges = ranges[np.isfinite(ranges)]
-    angles = angles[np.isfinite(ranges)]
-
-    k_smallest_idx = np.argpartition(ranges,num_smallest)
-    k_smallest_idx = k_smallest_idx[:num_smallest]
-
-    lidar_xy_car_frame = np.zeros((num_smallest,2))
-
-    lidar_xy_car_frame[:,0] = (ranges[k_smallest_idx] * np.cos(angles[k_smallest_idx]))
-    lidar_xy_car_frame[:,1] = (ranges[k_smallest_idx] * np.sin(angles[k_smallest_idx]))
-
-    lidar_xy_global = np.dot(lidar_xy_car_frame,R_mat_ego)
-
-    obs_array[10:] = lidar_xy_global.flatten()
-
-    return obs_array
 
 def sqn(env_fn, env_init, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0, 
         steps_per_epoch=4000, epochs=100, replay_size=int(1e6), gamma=0.99, 
@@ -222,9 +154,6 @@ def sqn(env_fn, env_init, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), se
     obs_dim = env.observation_space.shape[0]
     act_dim = env.action_space.n
 
-    # # Action limit for clamping: critically, assumes all dimensions share the same bound!
-    # act_limit = env.action_space.high[0]
-
     # Create actor-critic module and target networks
     ac = actor_critic(env.observation_space, env.action_space, alpha, **ac_kwargs)
     ac_targ = deepcopy(ac)
@@ -283,24 +212,6 @@ def sqn(env_fn, env_init, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), se
 
         return loss_q, q_info
 
-    # Set up function for computing SAC pi loss
-    # def compute_loss_pi(data):
-    #     o = data['obs']
-    #     pi, logp_pi = ac.pi(o)
-    #     q1_pi = ac.q1(o, pi)
-    #     q2_pi = ac.q2(o, pi)
-    #     q_pi = torch.min(q1_pi, q2_pi)
-
-    #     # Entropy-regularized policy loss
-    #     loss_pi = (alpha * logp_pi - q_pi).mean()
-
-    #     # Useful info for logging
-    #     pi_info = dict(LogPi=logp_pi.detach().numpy())
-
-    #     return loss_pi, pi_info
-
-    # Set up optimizers for policy and q-function
-    # pi_optimizer = Adam(ac.pi.parameters(), lr=lr)
     q_optimizer = Adam(q_params, lr=lr)
 
     # Set up model saving
@@ -315,24 +226,6 @@ def sqn(env_fn, env_init, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), se
 
         # Record things
         logger.store(LossQ=loss_q.item(), **q_info)
-
-        # Freeze Q-networks so you don't waste computational effort 
-        # computing gradients for them during the policy learning step.
-        # for p in q_params:
-            # p.requires_grad = False
-
-        # Next run one gradient descent step for pi.
-        # pi_optimizer.zero_grad()
-        # loss_pi, pi_info = compute_loss_pi(data)
-        # loss_pi.backward()
-        # pi_optimizer.step()
-
-        # Unfreeze Q-networks so you can optimize it at next DDPG step.
-        # for p in q_params:
-            # p.requires_grad = True
-
-        # Record things
-        # logger.store(LossPi=loss_pi.item(), **pi_info)
 
         # Finally, update target networks by polyak averaging.
         with torch.no_grad():
